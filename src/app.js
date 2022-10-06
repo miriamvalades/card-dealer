a9763f3580a40a270a06450237a2bec81fa58c04
miriamvalades <miriamex56@gmail.com>
2022-10-06 19:29:36 +0000

    card dealer

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  let suitvalue = generateRandomSuit();
+  document.querySelector(".suit").innerHTML = suitvalue;
+  document.getElementById("bottom").innerHTML = suitvalue;
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  if (suit[indexSuit] === "♦" || suit[indexSuit] === "♥") {
+    let color = document.getElementsByClassName("suit");
+    let colorArray = [...color];
+    colorArray.map(element => {
+      element.style.color = "red";
+      return element;
+    });
+    console.log(color);
+  }
+  return suit[indexSuit];
 };
